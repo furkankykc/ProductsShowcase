9274d7671533c41be18ff18724f37cffd388fb01
furkankykc <furkanfbr@gmail.com>
2019-03-06 18:38:24 +0300

    added url routing for index page

--- mailProduct/urls.py
+++ mailProduct/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic import RedirectView
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -22,7 +24,7 @@ from product import views
 
 urlpatterns = [
 
-                  path('', views.index, name='home'),
+                  path('', RedirectView.as_view(url='/page/dynamic'), name='home'),
                   path('admin/', admin.site.urls),
                   path('', include('product.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
